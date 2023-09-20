f2bb1d5da26d89dcfc469a0493bc2f6b5757a482
shohan <shohan.ece17@gmail.com>
2023-09-20 19:09:21 +0600

    begin loan calculator

---
+++ Loan calculator/script.js
@@ -0,0 +1,8 @@
+let loanAmount = document.getElementById('loan-amount');
+let loanAmountNumber = document.getElementById('loan-amount-in-numbers');
+
+function fetchLoanAmount() {
+   loanAmountNumber.value = loanAmount.value;
+   // console.log(loanAmountNumber);
+
+}
